db110f93b82501813f6a6a311b2f36b8a0ac5e19
Ubuntu <ubuntu@ip-172-31-33-61.eu-north-1.compute.internal>
2025-04-09 17:56:56 +0000

    requests sent rn

--- backend/backend/storage_backends.py
+++ backend/backend/storage_backends.py
@@ -1,17 +1,11 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class MediaStorage(S3Boto3Storage):
-    """
-    Storage class for media files (user uploaded content)
-    Stores files in the 'media' directory on S3
-    """
-    location = 'media'
-    file_overwrite = False
-
 class StaticStorage(S3Boto3Storage):
-    """
-    Storage class for static files (CSS, JS, etc.)
-    Stores files in the 'static' directory on S3
-    """
     location = 'static'
-    file_overwrite = True 
+    default_acl = 'public-read'
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
